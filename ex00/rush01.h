32751791667af873d0f150a162d4a6a3e42a1954
Victor Quetier <vquetier@z3r3p1.42lyon.fr>
2026-07-29 19:52:46 +0200

    dummy

---
+++ ex00/rush01.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vquetier <vquetier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/29 19:46:21 by vquetier          #+#    #+#             */
+/*   Updated: 2026/07/29 19:52:42 by vquetier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RUSH01_H
+# define RUSH01_H
+
+typedef struct	s_parsed
+{
+	int	size;
+	int	*constraint;
+	int	constraint_size;
+}	t_parsed;
+
+#endif
