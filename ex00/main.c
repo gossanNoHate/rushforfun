637eace0008b654369dac053928c56f0979d2603
Victor Quetier <vquetier@z3r3p1.42lyon.fr>
2026-07-29 19:58:38 +0200

    dummy

--- ex00/main.c
+++ ex00/main.c
@@ -6,11 +6,27 @@
 /*   By: jbenhass <jbenhass@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 19:46:21 by jbenhass          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2026/07/29 19:46:43 by jbenhass         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(void)
+=======
+/*   Updated: 2026/07/29 19:57:36 by vquetier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush01.h"
+
+
+
+int	main(int ac, char **av)
+>>>>>>> Stashed changes
 {
-	return	 (0);
+	t_parsed	*parsed;
+
+	parsed = parse_args(ac, av);
+	if (parsed == NULL)
+		return ()
 }
