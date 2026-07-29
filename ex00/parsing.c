867782002286b0fde0cd1492b1108b0b8ec88a12
Jilan Benhassine <jbenhass@z3r4p2.42lyon.fr>
2026-07-29 20:03:30 +0200

    Feat - zizi

--- ex00/parsing.c
+++ ex00/parsing.c
@@ -6,11 +6,21 @@
 /*   By: jbenhass <jbenhass@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 19:47:59 by jbenhass          #+#    #+#             */
-/*   Updated: 2026/07/29 19:52:50 by jbenhass         ###   ########lyon.fr   */
+/*   Updated: 2026/07/29 20:02:26 by jbenhass         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush01.h"
 
+t_parsed	*parse_args(int argc, char **args)
+{
+	int	*constraint;
+	int	size;
 
-int	converte_args
+	if (argc != 2 || !args[1])
+		return (NULL);
+	while (args[1][size])
+		size++;
+	
+	constraint = malloc(sizeof(int) * size)
+}
