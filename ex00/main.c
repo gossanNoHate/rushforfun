b6732e8fbc785b87aec6154604adb5d8df150574
Victor Quetier <vquetier@z3r3p1.42lyon.fr>
2026-07-29 20:24:51 +0200

    dummy

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: vquetier <vquetier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 19:46:21 by jbenhass          #+#    #+#             */
-/*   Updated: 2026/07/29 20:20:26 by vquetier         ###   ########.fr       */
+/*   Updated: 2026/07/29 20:24:26 by vquetier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,28 @@ int	raise_error()
 	return (1);
 }
 
+void	fill_with_zero(t_solution *sol)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < sol->parsed.size)
+	{
+		j = 0;
+		while (j < sol->parsed.size)
+			sol->solution[i][j++] = 0
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_solution	sol;
+
+	if (parse_args(ac, av, &(sol.parsed)))
+		return (raise_error());
+	fill_with_zero(&sol);
 	
 	return 0;
 }
