814bcfe09e5840f237776970570209617f6d9268
Victor Quetier <vquetier@z3r3p1.42lyon.fr>
2026-07-29 20:09:38 +0200

    dummy

--- ex00/main.c
+++ ex00/main.c
@@ -3,30 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbenhass <jbenhass@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vquetier <vquetier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 19:46:21 by jbenhass          #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2026/07/29 19:46:43 by jbenhass         ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
-
-int	main(void)
-=======
-/*   Updated: 2026/07/29 19:57:36 by vquetier         ###   ########.fr       */
+/*   Updated: 2026/07/29 20:09:18 by vquetier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush01.h"
 
+int	raise_error()
+{
+	write(2, "Error\n", 6);
+	return (1);
+}
 
+t_solution	*create_sol(t_parsed *parsed)
+{
+	t_solution	*sol;
+
+	sol = malloc(sizeof(t_solution));
+	if (sol == NULL)
+		return NULL;
+	sol->parsed = parsed
+	sol
+}
 
 int	main(int ac, char **av)
->>>>>>> Stashed changes
 {
 	t_parsed	*parsed;
 
 	parsed = parse_args(ac, av);
 	if (parsed == NULL)
-		return ()
+		return (raise_error());
+	
+	sol = create_sol();
 }
